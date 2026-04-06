c913e585b1d6baea7fcdfa7199fbc3b7dc74bf55
Sam Littler <samlittler57@gmail.com>
2026-04-06 12:24:57 +0100

    Add users service and test

--- src/Services/UsersService.php
+++ src/Services/UsersService.php
@@ -3,10 +3,75 @@
 namespace SamMakesCode\DummyUsers\Services;
 
 use GuzzleHttp\Client;
+use InvalidArgumentException;
+use SamMakesCode\DummyUsers\Exceptions\ModelNotFoundException;
+use SamMakesCode\DummyUsers\Models\User;
 
 class UsersService
 {
     public function __construct(
         private Client $client,
     ) {}
+
+    public function getById(int $id): User
+    {
+        if ($id <= 0) {
+            throw new InvalidArgumentException('Invalid user ID.');
+        }
+
+        $response = $this->client->get('users/' . $id);
+
+        if ($response->getStatusCode() === 400) {
+            throw new ModelNotFoundException('No user with that ID exists.');
+        }
+
+        $body = $response->getBody()->getContents();
+        $data = json_decode($body, true);
+        return $this->hydrateModelWithData($data);
+    }
+
+    public function getPage(int $page = 1, int $perPage = 25): array
+    {
+        $limit = $perPage;
+        $skip = ($page - 1) * $limit;
+
+        $uriTemplate = 'users?limit=%s&skip=%s';
+        $uri = sprintf($uriTemplate, $limit, $skip);
+
+        $response = $this->client->get($uri);
+        $body = $response->getBody()->getContents();
+        $data = json_decode($body, true);
+        $users = array_map(function ($user) {
+            return $this->hydrateModelWithData($user);
+        }, $data);
+        return $users;
+    }
+
+    public function create(
+        string $firstName,
+        string $lastName,
+        string $email,
+    ): int {
+        $response = $this->client->post('users/add', [
+            'json' => json_encode([
+                'firstName' => $firstName,
+                'lastName' => $lastName,
+                'email' => $email,
+            ]),
+        ]);
+
+        $contents = $response->getBody()->getContents();
+        $data = json_decode($contents, true);
+        return $this->hydrateModelWithData($data)->id;
+    }
+
+    private function hydrateModelWithData(array $data): User
+    {
+        return new User(
+            $data['id'],
+            $data['firstName'],
+            $data['lastName'],
+            $data['email'],
+        );
+    }
 }
--- tests/Services/UsersServiceTest.php
+++ tests/Services/UsersServiceTest.php
@@ -2,12 +2,116 @@
 
 namespace Tests\Services;
 
+use Faker\Factory;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use InvalidArgumentException;
+use Mockery;
 use PHPUnit\Framework\TestCase;
+use SamMakesCode\DummyUsers\Exceptions\ModelNotFoundException;
+use SamMakesCode\DummyUsers\Models\User;
+use SamMakesCode\DummyUsers\Services\UsersService;
 
 class UsersServiceTest extends TestCase
 {
-    public function testThatTrueIsTrue(): void
+    public function testCanGetByIdOptimalCase(): void
     {
-        $this->assertTrue(true);
+        $faker = Factory::create();
+
+        $testUserId = rand(1, 100000);
+        $testForename = $faker->firstName;
+        $testLastname = $faker->lastName;
+        $testEmail = $faker->email;
+        $response = new Response(
+            200,
+            [],
+            json_encode([
+                'id' => $testUserId,
+                'firstName' => $testForename,
+                'lastName' => $testLastname,
+                'email' => $testEmail,
+            ]),
+        );
+        $clientMock = Mockery::mock(Client::class);
+        $clientMock->expects('get')
+            ->once()
+            ->andReturn($response);
+
+        $usersService = new UsersService($clientMock);
+        $user = $usersService->getById(1);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($testUserId, $user->id);
+        $this->assertEquals($testForename, $user->firstName);
+        $this->assertEquals($testLastname, $user->lastName);
+        $this->assertEquals($testEmail, $user->email);
+    }
+
+    public function testCanGetByIdHandlesBadId(): void
+    {
+        $clientMock = Mockery::mock(Client::class);
+        $usersService = new UsersService($clientMock);
+        $this->expectException(InvalidArgumentException::class);
+        $usersService->getById(0);
+    }
+
+    public function testCanGetByIdThrowsException(): void
+    {
+        $response = new Response(400);
+        $clientMock = Mockery::mock(Client::class);
+        $clientMock->expects('get')
+            ->once()
+            ->andReturn($response);
+
+        $this->expectException(ModelNotFoundException::class);
+
+        $usersService = new UsersService($clientMock);
+        $usersService->getById(1);
+    }
+
+    public function testCanGetPageOptimalCase(): void
+    {
+        $faker = Factory::create();
+        $users = [];
+        for ($i = 0; $i < 25; $i++) {
+            $users[] = [
+                'id' => $i + 1,
+                'firstName' => $faker->firstName,
+                'lastName' => $faker->lastName,
+                'email' => $faker->email,
+            ];
+        }
+        $response = new Response(200, [], json_encode($users));
+        $clientMock = Mockery::mock(Client::class);
+        $clientMock->expects('get')
+            ->once()
+            ->andReturn($response);
+
+        $usersService = new UsersService($clientMock);
+        $users = $usersService->getPage();
+
+        $this->assertIsArray($users);
+        $this->assertCount(25, $users);
+    }
+
+    public function testCanCreate()
+    {
+        $faker = Factory::create();
+        $testId = rand(1, 1000000000);
+        $response = new Response(200, [], json_encode([
+            'id' => $testId,
+            'firstName' => $faker->firstName,
+            'lastName' => $faker->lastName,
+            'email' => $faker->email,
+        ]));
+        $clientMock = Mockery::mock(Client::class);
+        $clientMock->expects('post')
+            ->once()
+            ->andReturn($response);
+        $usersService = new UsersService($clientMock);
+        $response = $usersService->create('John', 'Smith', 'john@example.org');
+
+        $this->assertIsInt($response);
+        $this->assertEquals($testId, $response);
     }
 }
