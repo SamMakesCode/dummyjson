ba5c11c3078ef8672858c160954781dbd1a18af0
Sam Littler <samlittler57@gmail.com>
2026-04-06 12:31:15 +0100

    Add serialization test for model

---
+++ tests/Models/UsersTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Models;
+
+use Faker\Factory;
+use PHPUnit\Framework\TestCase;
+use SamMakesCode\DummyUsers\Models\User;
+
+class UsersTest extends TestCase
+{
+    public function testThatModelCanSerializeCorrectly(): void
+    {
+        $faker = Factory::create();
+
+        $testId = $faker->numberBetween();
+        $testFirstName = $faker->firstName;
+        $testLastName = $faker->lastName;
+        $testEmail = $faker->email;
+
+        $user = new User(
+            $testId,
+            $testFirstName,
+            $testLastName,
+            $testEmail,
+        );
+
+        $template = '{"id":%s,"firstName":"%s","lastName":"%s","email":"%s"}';
+        $actual = sprintf($template, $testId, $testFirstName, $testLastName, $testEmail);
+        $this->assertEquals($actual, json_encode($user));
+    }
+}
